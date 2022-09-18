b71c891f48380861d5cca73e48fd3402035cfd8f
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-09-18 16:13:05 -0300

    Using Unsplash API to show image of the city

--- js/script.js
+++ js/script.js
@@ -16,14 +16,27 @@ const wind = document.querySelector('#wind span');
 
 const errorMessage = document.querySelector('#error-message');
 
+const body = document.querySelector('body');
+
 const showErrorMessage = () => {
     errorMessage.classList.remove('hide');
     weatherData.classList.add('hide');
 }
 
+const resetBackground = () => {
+    body.style.background = 'linear-gradient(180deg, #594cee 0%, #aaeaff 100%)';
+}
+
 const getWeatherData = async (city) => {
     const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
     const resp = await fetch(apiWeatherUrl);
+    const data = await resp.json();
+    return data;
+}
+
+const getImage = async (city) => {
+    const apiUnsplashUrl = `https://api.unsplash.com/photos/random?query=${city}&orientation=landscape&topics=travel,history&client_id=TEzC0yPI-sdHbnwGSV5xBH1PFRytdPww4KBiK--cGSE`;
+    const resp = await fetch(apiUnsplashUrl);
     const data = await resp.json();
     return data;
 }
@@ -44,14 +57,38 @@ const showWeatherData = async (city) => {
     catch {
         showErrorMessage();
     }
+
 }
 
-const search = (e) => {
-    e.preventDefault();
-    if (!cityInput.value.trim() == '') {
-        const city = cityInput.value;
-        showWeatherData(city);
+
+const changeBackground = async (city) => {
+    try {
+        const data = await getImage(city);
+        const image = data.urls.regular;
+        body.style.background = 'none';
+        body.style.backgroundImage = `url(${image})`;
+        body.style.backgroundPosition = 'center';
+        body.style.backgroundSize = 'cover';
     }
+    catch {
+        resetBackground();
+    }
+}
+
+
+
+const search = async (e) => {
+    try {
+        e.preventDefault();
+        if (!cityInput.value.trim() == '') {
+            const city = cityInput.value;
+            await showWeatherData(city);
+            await changeBackground(city);
+        }
+    }
+    catch {
+
+    }   
 }
 
 searchBtn.addEventListener('click', search);
